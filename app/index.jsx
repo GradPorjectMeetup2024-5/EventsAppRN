653ef6ae7ec2582fc93881062c9c98d0597cac6d
Zena <zenanusair2002@gmail.com>
2024-10-06 23:41:34 +0300

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>MeJo!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}>
+        HOME
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
