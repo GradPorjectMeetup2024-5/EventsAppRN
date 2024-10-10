211ea7f637111d5e990a76894bf9ba80cb5504bb
abdalla <abdalla.wohoush@gmail.com>
2024-10-10 15:20:17 +0300

    skeleton for auth file
    use the following for the code to work:
    npm install react-native-svg
    npm install @react-navigation/stack

--- app/index.jsx
+++ app/index.jsx
@@ -2,16 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
+import LogIn from './(auth)/log-in';
+import SignUp from './(auth)/sign-up';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>MeJo!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: 'blue' }}>
-        HOME
-      </Link>
-    </View>
-  );
+  return <LogIn />;
 }
 
 const styles = StyleSheet.create({
